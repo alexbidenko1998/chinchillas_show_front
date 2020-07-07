329a06b745e2800f34b63caa52dcb0064138f371
Alexander <alexbidenko1998@gmail.com>
2020-07-07 20:20:06 +0300

    Update

---
+++ assets/scripts/colorToString.js
@@ -0,0 +1,489 @@
+export default (color) => {
+  if (!color) return ''
+  const {
+    standard,
+    white,
+    mosaic,
+    beige,
+    violet,
+    sapphire,
+    angora,
+    ebony,
+    velvet,
+    pearl,
+    california,
+    rex,
+    lova,
+    german,
+    blue,
+    fur
+  } = color
+
+  const WhoIsLeader = () => {
+    if (
+      white === 'no' &&
+      beige !== 'no' &&
+      ebony !== 'homoebony' &&
+      ebony !== 'no'
+    )
+      return 'pastel'
+    else if (ebony !== 'no' && ebony !== 'carrier') return 'ebony'
+    else if (
+      (violet === 'yes' && sapphire === 'yes') ||
+      (german === 'yes' && sapphire === 'yes') ||
+      (violet === 'yes' && german === 'yes')
+    )
+      return 'diamond'
+    else if (violet === 'yes' || sapphire === 'yes' || german === 'yes')
+      return 'notDiamond'
+    else if (velvet === 'yes') return 'velvet'
+    else return 'other'
+  }
+
+  const LEADER = WhoIsLeader()
+
+  const MorFEnd = (point = 3) => {
+    if (point === 1) {
+      if (LEADER === 'pastel') return 'ая'
+      else return 'ой'
+    } else if (point === 2) {
+      if (LEADER === 'pastel') return 'ая'
+      else return 'ий'
+    } else if (LEADER === 'pastel') return 'ая'
+    else return 'ый'
+  }
+
+  const ebonyName = (ebony) => {
+    let ebonyName = ''
+    switch (ebony) {
+      case 'carrier':
+        ebonyName = 'носитель эбони'
+        break
+      case 'light':
+        ebonyName = 'эбони светлый'
+        break
+      case 'middle':
+        ebonyName = 'эбони средний'
+        break
+      case 'dark':
+        ebonyName = 'эбони темный'
+        break
+      case 'exdark':
+        ebonyName = 'эбони экстра темный'
+        break
+      case 'homoebony':
+        ebonyName = 'гомоэбони'
+        break
+    }
+    return ebonyName
+  }
+
+  const isEbony = () => {
+    if (ebony !== 'no') {
+      if (beige === 'no') return ebonyName(ebony) + ' '
+      else return ''
+    } else return ''
+  }
+
+  const isBeige = () => {
+    if (beige !== 'no') {
+      if (ebony === 'no' && white === 'no') {
+        if (
+          beige === 'yes' &&
+          !(
+            velvet === 'yes' &&
+            white !== 'yes' &&
+            violet !== 'yes' &&
+            sapphire !== 'yes' &&
+            (ebony === 'no' || ebony === 'carrier') &&
+            pearl !== 'yes' &&
+            california !== 'yes' &&
+            lova !== 'yes' &&
+            german !== 'yes' &&
+            blue !== 'yes'
+          )
+        )
+          return 'гетеробежевый '
+        else if (beige === 'double') return 'гомобежевый '
+        else return ''
+      } else return ''
+    } else return ''
+  }
+
+  const isWHITEPINK = () => {
+    if (white === 'yes' && beige === 'no' && ebony !== 'homoebony') {
+      return 'белый '
+    } else if (white === 'yes' && beige !== 'no' && ebony !== 'homoebony') {
+      switch (beige) {
+        case 'yes':
+          return 'бело-розовый '
+        case 'double':
+          return 'бело-розовый гомобежевый '
+      }
+    } else if (white === 'yes' && beige !== 'no' && ebony === 'homoebony') {
+      switch (beige) {
+        case 'yes':
+          return 'белый шоколад '
+        case 'double':
+          return 'белый гомобежевый шоколад '
+      }
+    } else return ''
+  }
+
+  const isPASTEL = () => {
+    if (white === 'no' && beige === 'yes' && ebony !== 'homoebony') {
+      switch (ebony) {
+        case 'homoebony':
+          return 'шоколад '
+        case 'exdark':
+          return 'пастель экстра темная '
+        case 'dark':
+          return 'пастель темная '
+        case 'middle':
+          return 'пастель средняя '
+        case 'light':
+          return 'пастель светлая '
+        default:
+          return ''
+      }
+    } else if (white === 'no' && beige === 'double' && ebony !== 'homoebony') {
+      switch (ebony) {
+        case 'homoebony':
+          return 'гомобежевый шоколад '
+        case 'exdark':
+          return 'гомобежевая пастель экстра темная '
+        case 'dark':
+          return 'гомобежевая пастель темная '
+        case 'middle':
+          return 'гомобежевая пастель средняя '
+        case 'light':
+          return 'гомобежевая пастель светлая '
+        default:
+          return ''
+      }
+    } else return ''
+  }
+
+  const isStandart = () => {
+    if (standard === 'yes') return 'стандарт '
+    else return ''
+  }
+
+  const isMosaic = () => {
+    if (mosaic === 'yes') return 'мозаика '
+    else return ''
+  }
+
+  const isViolet = (POSITION) => {
+    if (violet !== 'no' && sapphire !== 'yes' && german !== 'yes') {
+      if (LEADER !== 'notDiamond') {
+        if (POSITION === 1) {
+          switch (violet) {
+            case 'yes':
+              return `фиолетов${MorFEnd()} `
+            default:
+              return ''
+          }
+        } else {
+          switch (violet) {
+            case 'carrier':
+              return 'носитель фиолета '
+            case '67%':
+              return '67% носитель фиолета '
+            case '50%':
+              return '50% носитель фиолета '
+            default:
+              return ''
+          }
+        }
+      } else if (POSITION === 1) return 'фиолет '
+      else return ''
+    } else if (POSITION === 2) {
+      switch (violet) {
+        case 'carrier':
+          return 'носитель фиолета '
+        case '67%':
+          return '67% носитель фиолета '
+        case '50%':
+          return '50% носитель фиолета '
+        default:
+          return ''
+      }
+    } else return ''
+  }
+
+  const isSapphire = (POSITION) => {
+    if (sapphire !== 'no' && violet !== 'yes' && german !== 'yes') {
+      if (LEADER !== 'notDiamond') {
+        if (POSITION === 1) {
+          switch (sapphire) {
+            case 'yes':
+              return `сапфиров${MorFEnd()} `
+            default:
+              return ''
+          }
+        } else {
+          switch (sapphire) {
+            case 'carrier':
+              return 'носитель сапфира '
+            case '67%':
+              return '67% носитель сапфира '
+            case '50%':
+              return '50% носитель сапфира '
+            default:
+              return ''
+          }
+        }
+      } else if (POSITION === 1) return 'сапфир '
+      else return ''
+    } else if (POSITION === 2) {
+      switch (sapphire) {
+        case 'carrier':
+          return 'носитель сапфира '
+        case '67%':
+          return '67% носитель сапфира '
+        case '50%':
+          return '50% носитель сапфира '
+        default:
+          return ''
+      }
+    } else return ''
+  }
+
+  const isDIAMOND = () => {
+    if (sapphire === 'yes' && violet === 'yes' && german === 'yes') {
+      if (LEADER === 'diamond') {
+        return 'триплбриллиант '
+      } else return `триплбриллиантов${MorFEnd()} `
+    } else if (sapphire === 'yes' && violet === 'yes' && german !== 'yes') {
+      if (LEADER === 'diamond') {
+        return 'бриллиант '
+      } else return `бриллиантов${MorFEnd()} `
+    } else if (sapphire === 'yes' && violet !== 'yes' && german === 'yes') {
+      if (LEADER === 'diamond') {
+        return 'немецкий бриллиант '
+      } else return `немецк${MorFEnd(2)} бриллиантов${MorFEnd()} `
+    } else if (sapphire !== 'yes' && violet === 'yes' && german === 'yes') {
+      if (LEADER === 'diamond') {
+        return 'double фиолет '
+      } else return `двойн${MorFEnd(2)} фиолетов${MorFEnd()} `
+    } else return ''
+  }
+
+  const isAngora = (POSITION) => {
+    if (angora !== 'no') {
+      if (POSITION === 1) {
+        switch (angora) {
+          case 'yes':
+            return 'КПА '
+          default:
+            return ''
+        }
+      } else {
+        switch (angora) {
+          case 'carrier':
+            return 'НКПА '
+          case '67%':
+            return '67% НКПА '
+          case '50%':
+            return '50% НКПА '
+          default:
+            return ''
+        }
+      }
+    } else return ''
+  }
+
+  const isVelvet = () => {
+    if (velvet === 'yes') {
+      if (
+        beige === 'yes' &&
+        white !== 'yes' &&
+        violet !== 'yes' &&
+        sapphire !== 'yes' &&
+        (ebony === 'no' || ebony === 'carrier') &&
+        pearl !== 'yes' &&
+        california !== 'yes' &&
+        lova !== 'yes' &&
+        german !== 'yes' &&
+        blue !== 'yes'
+      ) {
+        return 'коричневый бархат '
+      } else if (LEADER === 'velvet') return 'бархат '
+      else return `бархатн${MorFEnd()} `
+    } else return ''
+  }
+
+  const isPearl = (POSITION) => {
+    if (pearl !== 'no') {
+      if (POSITION === 1) {
+        switch (pearl) {
+          case 'yes':
+            return 'жемчуг '
+          default:
+            return ''
+        }
+      } else {
+        switch (pearl) {
+          case 'carrier':
+            return 'носитель жемчуга '
+          case '67%':
+            return '67% носитель жемчуга '
+          case '50%':
+            return '50% носитель жемчуга '
+          default:
+            return ''
+        }
+      }
+    } else return ''
+  }
+
+  const isCalifornia = (POSITION) => {
+    if (california !== 'no') {
+      if (POSITION === 1) {
+        switch (california) {
+          case 'yes':
+            return `калифорнийск${MorFEnd(2)} бел${MorFEnd()} `
+          default:
+            return ''
+        }
+      } else {
+        switch (california) {
+          case 'carrier':
+            return 'носитель калифорнийского белого '
+          case '67%':
+            return '67% носитель калифорнийского белого '
+          case '50%':
+            return '50% носитель калифорнийского белого '
+          default:
+            return ''
+        }
+      }
+    } else return ''
+  }
+
+  const isRex = (POSITION) => {
+    if (rex !== 'no') {
+      if (POSITION === 1) {
+        switch (rex) {
+          case 'yes':
+            return 'рекс '
+          default:
+            return ''
+        }
+      } else {
+        switch (rex) {
+          case 'carrier':
+            return 'носитель рекса '
+          case '67%':
+            return '67% носитель рекса '
+          case '50%':
+            return '50% носитель рекса '
+          default:
+            return ''
+        }
+      }
+    } else return ''
+  }
+
+  const isLova = (POSITION) => {
+    if (lova !== 'no') {
+      if (POSITION === 1) {
+        switch (lova) {
+          case 'yes':
+            return 'белый Лова '
+          default:
+            return ''
+        }
+      } else {
+        switch (lova) {
+          case 'carrier':
+            return 'носитель Лова '
+          case '67%':
+            return '67% носитель Лова '
+          case '50%':
+            return '50% носитель Лова '
+          default:
+            return ''
+        }
+      }
+    } else return ''
+  }
+
+  const isGerman = (POSITION) => {
+    if (german !== 'no' && violet !== 'yes' && sapphire !== 'yes') {
+      if (LEADER !== 'notDiamond') {
+        if (POSITION === 1) {
+          switch (german) {
+            case 'yes':
+              return `немецк${MorFEnd(2)} фиолетов${MorFEnd()} `
+            default:
+              return ''
+          }
+        } else {
+          switch (german) {
+            case 'carrier':
+              return 'носитель немецкого фиолета '
+            case '67%':
+              return '67% носитель немецкого фиолета '
+            case '50%':
+              return '50% носитель немецкого фиолета '
+            default:
+              return ''
+          }
+        }
+      } else if (POSITION === 1) return 'немецкий фиолет '
+      else return ''
+    } else return ''
+  }
+
+  const isBlue = () => {
+    if (blue === 'yes') return 'блю слейт '
+    else return ''
+  }
+
+  const isFur = () => {
+    if (fur === 'yes') return `мехов${MorFEnd(1)} `
+    else return ''
+  }
+
+  let fullChinColorName =
+    isFur() +
+    isStandart() +
+    isWHITEPINK() +
+    isBeige() +
+    isVelvet() +
+    isViolet(1) +
+    isSapphire(1) +
+    isGerman(1) +
+    isCalifornia(1) +
+    isLova(1) +
+    isRex(1) +
+    isDIAMOND() +
+    isPearl(1) +
+    isPASTEL() +
+    isEbony() +
+    isAngora(1) +
+    isBlue() +
+    isMosaic() +
+    isViolet(2) +
+    isSapphire(2) +
+    isGerman(2) +
+    isPearl(2) +
+    isAngora(2) +
+    isCalifornia(2) +
+    isRex(2) +
+    isLova(2)
+
+  if (fullChinColorName.substring(0, 10) === 'бриллиант ') {
+    fullChinColorName = 'голубой ' + fullChinColorName
+  } else if (fullChinColorName.substring(0, 7) === 'жемчуг ') {
+    fullChinColorName = 'черный ' + fullChinColorName
+  } else if (fullChinColorName.substring(0, 7) === 'бархат ') {
+    fullChinColorName = 'черный ' + fullChinColorName
+  }
+
+  fullChinColorName =
+    fullChinColorName[0].toUpperCase() + fullChinColorName.slice(1)
+  return fullChinColorName
+}
--- pages/profile/chinchillas/view/index.vue
+++ pages/profile/chinchillas/view/index.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="viewPage baseContainer">
     <template v-if="data">
-      {{ data.name }}
+      <div>{{ data.name }}</div>
+      <div>{{ colorString }}</div>
+      <div class="viewPage__photosList">
+        <div
+          v-for="photo in data.photos"
+          :key="photo.id"
+          class="viewPage__photoContainer"
+        >
+          <img
+            :src="
+              `https://api.chinchillas-show.com/photos/chinchillas/${data.owner_id}/${data.id}/${photo.name}`
+            "
+            :alt="data.name"
+            class="viewPage__photo"
+          />
+          <button class="viewPage__delete" @click="deletePhoto(photo.id)">
+            X
+          </button>
+        </div>
+      </div>
+      <input
+        type="file"
+        multiple
+        accept="image/*"
+        placeholder="Фотографии"
+        @change="uploadPhotos"
+      />
       <nuxt-link
         v-if="userId === data.owner_id"
         :to="`/profile/chinchillas/color?id=${chinchillaId}`"
@@ -14,6 +40,7 @@
 </template>
 
 <script>
+import colorToString from '~/assets/scripts/colorToString'
 import PedigreeTree from '~/components/PedigreeTree/PedigreeTree.vue'
 import BaseSpinner from '~/components/BaseSpinner/BaseSpinner.vue'
 
@@ -33,10 +60,34 @@ export default {
     }
   },
 
+  computed: {
+    colorString() {
+      return this.data ? colorToString(this.data.color) : ''
+    }
+  },
+
   created() {
     this.$axios
       .$get(`chinchilla/details/${this.chinchillaId}`)
       .then((data) => (this.data = data))
+  },
+
+  methods: {
+    deletePhoto(photoId) {
+      this.$axios.$delete(`/photo/chinchilla/${photoId}`).then(() => {
+        this.data.photos = this.data.photos.filter((el) => el.id !== photoId)
+      })
+    },
+    uploadPhotos(event) {
+      const requests = [...event.target.files].map((file) => {
+        const formData = new FormData()
+        formData.append('photo', file)
+        return this.$axios.$post(`/photo/chinchilla/${this.data.id}`, formData)
+      })
+      Promise.all(requests).then((data) => {
+        this.data.photos = this.data.photos.concat(data)
+      })
+    }
   }
 }
 </script>
@@ -50,5 +101,27 @@ export default {
   align-items: center;
   justify-content: center;
   flex-direction: column;
+
+  &__photosList {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  &__photoContainer {
+    width: 200px;
+    height: 200px;
+    position: relative;
+  }
+
+  &__photo {
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  &__delete {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
 }
 </style>
