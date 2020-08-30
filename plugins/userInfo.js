c6f0868d8da9603c2b091504267279e0d77e9753
Alexander <alexbidenko1998@gmail.com>
2020-08-30 13:59:35 +0300

    Update

--- plugins/userInfo.js
+++ plugins/userInfo.js
@@ -1,8 +1,9 @@
+import requestIp from 'request-ip'
 import Mutations from '~/store/mutations.type'
 
 export default async ({ store, req }) => {
   if (process.server) {
-    const ip = req.connection.remoteAddress || req.socket.remoteAddress
+    const ip = requestIp.getClientIp(req)
     const response = await fetch(`http://www.geoplugin.net/json.gp?ip=${ip}`)
     store.commit(
       Mutations.SET_COUNTRY,
