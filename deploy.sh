fdf9093a47045fde4c1bcc69c274f021dc8eb242
Alexander <alexbidenko1998@gmail.com>
2021-01-08 16:23:52 +0300

    Update

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ docker-compose down
 docker-compose up --build -d
 rm -rf /etc/nginx/conf.d/chinchillas-show.conf || true
 cp chinchillas-show.conf /etc/nginx/conf.d
-system nginx reload
+service nginx reload
