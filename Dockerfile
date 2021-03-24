e0f0001fa544ee7ed0196cde924e187a031fab3c
Alexander <alexbidenko1998@gmail.com>
2021-03-24 23:40:57 +0300

    Update deployment configuration

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
-FROM node:12-alpine
+FROM node:15-alpine
 WORKDIR /app
 COPY . .
 RUN \
   yarn install && \
   yarn build
-EXPOSE 3333
 CMD ["yarn", "start"]
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-docker-compose down
-docker system prune --all --force
-docker-compose up --build -d
-rm -rf /etc/nginx/conf.d/chinchillas-show.conf || true
-cp .nginx/chinchillas-show.conf /etc/nginx/conf.d
-service nginx reload
